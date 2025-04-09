dd70dd7e8eee00dacc17180ead1769647b7b0b03
Nadia <nadja26714@gmail.com>
2025-04-09 17:54:06 +0300

    js homework

---
+++ script.js
@@ -0,0 +1,30 @@
+const person = "Nadia";
+console.log (typeof person)
+
+const age = 29;
+console.log (typeof age)
+
+const isSheGirl = true;
+console.log (typeof isSheGirl)
+
+let x
+console.log (x)
+
+const y = null;
+console.log (y)
+
+const sweets = ["buiscuit", "candypop", "icecream"];
+console.log (sweets[0]);
+
+const animal = {
+
+    type: "cat",
+
+    age: 2,
+
+    isDangerous: false
+};
+
+console.log (animal)
+
+//Если значению изменить одну переменную, то это не повлияет на другие переменные. А данные по ссылке хранят объект, и тогда там изменения одной переменной повлияют на другие.
