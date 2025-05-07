247283936df02abab0fa28dd63df5c3478386567
Nadia <nadja26714@gmail.com>
2025-05-07 18:56:49 +0300

    hw

---
+++ hw4/script.js
@@ -0,0 +1,22 @@
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+} 
+
+let i = 1;
+while (i < 101) {
+    console.log(i);
+   
+    i++;
+}
+
+let number = 100;
+ 
+Prime:
+for (let i = 2; i <= number; i++) { 
+ 
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue Prime; 
+  }
+ 
+  console.log( i ); 
+}
