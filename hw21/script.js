23a66f2928524f79cc076b9b1caf993227e0149f
Nadia <nadja26714@gmail.com>
2025-09-23 17:17:07 +0300

    hw21

---
+++ hw21/script.js
@@ -0,0 +1,45 @@
+function safeDivide(a, b) {
+  try {
+    if (b === 0) {
+      throw new Error("Деление на ноль невозможно");
+    }
+    return a / b;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+console.log(safeDivide(10, 2)); 
+console.log(safeDivide(10, 0)); 
+
+
+
+function transformJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch {
+    console.log("Ошибка: неправильный формат JSON");
+    return null;
+  }
+}
+
+console.log(transformJSON('{"name":"Анна"}')); 
+console.log(transformJSON('невалидный JSON'))
+
+
+
+function checkAccess(age) {
+  try {
+    if (age < 18) {
+      throw new Error("Доступ запрещен");
+    }
+    console.log("Доступ разрешен");
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+checkAccess(20); 
+checkAccess(15); 
+
+
